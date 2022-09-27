9de632dafd4dec5a8e820401efa7db5c5546c5f1
kimnamsun <nsunny0908@naver.com>
2022-09-28 00:35:12 +0900

    :heavy_plus_sign: recoil 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import type { AppProps } from "next/app";
+import { RecoilRoot } from 'recoil';
 
 function MoyeoApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
 
 export default MoyeoApp;
