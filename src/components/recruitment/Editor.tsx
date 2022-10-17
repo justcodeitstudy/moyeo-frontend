73fcc0e9621777b520e9ab4e5565d564e2e18a7b
ssabi <fhy1930@gmail.com>
2022-10-17 13:17:08 +0900

    feature: Editor 컴포넌트 구현

---
+++ src/components/recruitment/Editor.tsx
@@ -0,0 +1,37 @@
+import React, { memo, useRef } from "react";
+import { Editor as TuiEditor } from "@toast-ui/react-editor";
+import colorSyntax from "@toast-ui/editor-plugin-color-syntax";
+
+import "@toast-ui/editor/dist/toastui-editor.css";
+import "@toast-ui/editor/dist/i18n/ko-kr";
+import "tui-color-picker/dist/tui-color-picker.css";
+import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css";
+
+const Editor = () => {
+  const editorRef = useRef<TuiEditor>(null);
+  const toolbarItems = [
+    ["heading", "bold", "italic", "strike"],
+    ["hr"],
+    ["ul", "ol", "task"],
+    ["table", "link"],
+    ["code", "codeblock"],
+  ];
+  console.log("Recruitment");
+  return (
+    <TuiEditor
+      ref={editorRef}
+      previewStyle="tab"
+      previewHighlight={true}
+      initialEditType="wysiwyg"
+      initialValue=" "
+      height="500px"
+      language="ko-KR"
+      toolbarItems={toolbarItems}
+      usageStatistics={false}
+      useCommandShortcut={false}
+      plugins={[colorSyntax]}
+    />
+  );
+};
+
+export default memo(Editor);
