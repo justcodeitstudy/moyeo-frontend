6dd8590c02945d725febadd1f28758c9f5ef54b5
ssabi <fhy1930@gmail.com>
2022-10-17 21:15:16 +0900

    chore: 코드리뷰 반영

--- src/components/common/Section.tsx
+++ src/components/common/Section.tsx
@@ -2,24 +2,16 @@ import React from "react";
 import styled from "styled-components";
 
 export interface SectionProps {
-  margin?: string;
-  padding?: string;
   children?: React.ReactNode;
 }
 
-const Section = ({ margin, padding, children }: SectionProps) => {
-  return (
-    <Wrapper margin={margin} padding={padding}>
-      {children}
-    </Wrapper>
-  );
+const Section = ({ children }: SectionProps) => {
+  return <Wrapper>{children}</Wrapper>;
 };
 
 export default Section;
 
-const Wrapper = styled.div<SectionProps>`
-  margin: ${({ margin }) => margin || 0};
-  padding: ${({ padding }) => padding || 0};
+const Wrapper = styled.section<SectionProps>`
   background-color: ${({ theme }) => theme.colors.general["white"]};
   border: 1px solid ${({ theme }) => theme.colors.general["300"]};
   border-radius: 8px;
