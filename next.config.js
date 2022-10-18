9be0233ac2f2fee0b301b8b400b96746a087fec5
ssabi <fhy1930@gmail.com>
2022-10-18 19:48:04 +0900

    chore: next.config.js styled component 설정 추가

--- next.config.js
+++ next.config.js
@@ -9,6 +9,15 @@ const nextConfig = {
     });
     return config;
   },
+  compiler: {
+    styledComponents:
+      true |
+      {
+        ssr: true,
+        displayName: true,
+        preprocess: false,
+      },
+  },
 };
 
 module.exports = nextConfig;
