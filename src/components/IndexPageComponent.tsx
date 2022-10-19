e755c5ab98dff0f48046b0ff2f0b13e24aca1f73
kimnamsun <nsunny0908@naver.com>
2022-10-19 21:21:51 +0900

    :sparkles: feat: PostCard Component

--- src/components/IndexPageComponent.tsx
+++ src/components/IndexPageComponent.tsx
@@ -1,9 +1,37 @@
+import { Chip, Icon, Theme } from "jci-moyeo-design-system";
 import React from "react";
+import styled from "styled-components";
+import { PostCard } from "./common/PostCard";
 
 export const IndexPageComponent = () => {
+  const post = {
+    title: "태블릿 펜을 이용한 그림심리 검사앱",
+    isScrap: true,
+    content: "작성일 2022.00.00 · 조회 33",
+    skills: ["php", "nodejs", "python"],
+  };
+
   return (
     <>
       <span>main</span>
+      <PostCard
+        title={
+          <>
+            <span>{post?.title}</span>
+            {post?.isScrap && (
+              <Icon name="scrapOn" color={Theme.colors.primary[500]} />
+            )}
+          </>
+        }
+        content={post?.content}
+        footer={post.skills.map((skill) => {
+          return <StyledChip key={skill} color="basic" label={skill} />;
+        })}
+      />
     </>
   );
 };
+
+const StyledChip = styled(Chip)`
+  margin-right: 40px;
+`;
---
+++ src/components/common/PostCard.tsx
@@ -0,0 +1,54 @@
+import { Card } from "jci-moyeo-design-system";
+import React from "react";
+import styled from "styled-components";
+
+interface PostCardProps {
+  title?: React.ReactNode;
+  content?: string;
+  footer?: React.ReactNode;
+}
+
+export const PostCard = ({ title, content, footer }: PostCardProps) => {
+  return (
+    <StyledCard>
+      <StyledCardTitle>{title}</StyledCardTitle>
+      <StyledCardContent>{content}</StyledCardContent>
+      <Divider />
+      <StyledCardFooter>{footer}</StyledCardFooter>
+    </StyledCard>
+  );
+};
+
+const StyledCard = styled(Card)`
+  width: 486px;
+
+  @media (max-width: 767px) {
+    width: 343px;
+  }
+`;
+
+const StyledCardTitle = styled("header")`
+  ${({ theme }) => theme.typography.header3};
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+  height: 56px;
+`;
+
+const StyledCardContent = styled("div")`
+  ${({ theme }) => theme.typography.sm};
+  color: ${({ theme }) => theme.colors.general[600]};
+  height: 22px;
+`;
+
+const StyledCardFooter = styled("footer")`
+  height: 36px;
+  display: flex;
+  align-items: center;
+`;
+
+const Divider = styled("hr")`
+  background-color: ${({ theme }) => theme.colors.black[100]};
+  border: none;
+  height: 1px;
+`;
