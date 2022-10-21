c7c130287c2821def468899b830368eedc1bceb3
kimnamsun <nsunny0908@naver.com>
2022-10-22 01:54:56 +0900

    revert

--- src/components/IndexPageComponent.tsx
+++ src/components/IndexPageComponent.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { Search } from "./common/main/Search";
 
 export const IndexPageComponent = () => {
   return (
     <>
-      {/* <span>main</span> */}
-      <Search />
+      <span>main</span>
     </>
   );
 };
