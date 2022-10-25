30cb69d00da969637dabf4ee0d998d6b19744254
ssabii <fhy1930@gmail.com>
2022-10-25 23:59:15 +0900

    chore: Select Option 파일 분리

---
+++ src/components/post/SelectOptions.ts
@@ -0,0 +1,138 @@
+// 모집 구분
+export const typeOptions = [
+  {
+    id: 1,
+    value: "프로젝트",
+    label: "프로젝트",
+  },
+  {
+    id: 2,
+    value: "스터디",
+    label: "스터디",
+  },
+  {
+    id: 3,
+    value: "미지정",
+    label: "미지정",
+  },
+];
+
+// 진행 방식
+export const progressOptions = [
+  {
+    id: 1,
+    value: "온라인",
+    label: "온라인",
+  },
+  {
+    id: 2,
+    value: "오프라인",
+    label: "오프라인",
+  },
+];
+
+// 모집 분야
+export const fieldOptions = [
+  {
+    id: 1,
+    value: "백엔드",
+    label: "백엔드",
+  },
+  {
+    id: 2,
+    value: "프론트엔드",
+    label: "프론트엔드",
+  },
+  {
+    id: 3,
+    value: "디자인",
+    label: "디자인",
+  },
+  {
+    id: 4,
+    value: "기획",
+    label: "기획",
+  },
+  {
+    id: 5,
+    value: "기타",
+    label: "기타",
+  },
+];
+
+export const personnelOptions = [
+  {
+    id: 1,
+    value: "미지정",
+    label: "미지정",
+  },
+  {
+    id: 2,
+    value: "1명",
+    label: "1명",
+  },
+  {
+    id: 3,
+    value: "2명",
+    label: "2명",
+  },
+  {
+    id: 4,
+    value: "3명",
+    label: "3명",
+  },
+  {
+    id: 5,
+    value: "4명",
+    label: "4명",
+  },
+  {
+    id: 6,
+    value: "5명",
+    label: "5명",
+  },
+  {
+    id: 7,
+    value: "6명",
+    label: "6명",
+  },
+  {
+    id: 8,
+    value: "7명",
+    label: "7명",
+  },
+  {
+    id: 9,
+    value: "8명",
+    label: "8명",
+  },
+  {
+    id: 10,
+    value: "9명",
+    label: "9명",
+  },
+  {
+    id: 2,
+    value: "10명",
+    label: "10명",
+  },
+];
+
+// 기술 태그
+export const skillOptions = [
+  {
+    id: 1,
+    value: "javascript",
+    label: "javascript",
+  },
+  {
+    id: 2,
+    value: "typescript",
+    label: "typescript",
+  },
+  {
+    id: 3,
+    value: "react",
+    label: "react",
+  },
+];
