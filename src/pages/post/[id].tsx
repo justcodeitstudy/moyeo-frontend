c99f15652c65ede0ae48c2f0956f8bd39a81d9ca
ssabii <fhy1930@gmail.com>
2022-10-30 08:19:53 +0900

    feat: 모집글 페이지 레이아웃 적용

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import PostViewer from "components/post/PostViewer";
+import { NextPageWithLayout } from "pages/_app";
+import { DefaultLayout } from "layouts/DefaultLayout";
 
-const Post = () => {
+const Post: NextPageWithLayout = () => {
   return <PostViewer />;
   // return <PostEditor />;
+};
+
+Post.getLayout = (page) => {
+  return <DefaultLayout>{page}</DefaultLayout>;
 };
 
 export default Post;
